2904327d15550d7ee8bb0ff12dff2d243f99fa8e
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-19 21:50:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
