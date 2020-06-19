6731bb8c5a0190dae34b343f7a0f2a4779fe8ac6
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-19 21:53:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten() {
   return kittens.pop()
 }
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
